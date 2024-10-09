9b7d8f0e4346ecaa1965e133301d6b412d8e7322
jill <127872693+jill-7@users.noreply.github.com>
2024-10-10 02:58:36 +0300

    fetched data from the api and made several changes

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import Weather from "./Weather";
 function App() {
   return (
     <div className="App">
-      <Weather />
-      <p>This project was coded by <a href="https://github.com/jill-7">Jilloh</a> and is <a href="#">open sourced on Github</a>and <a href="#">hosted by Netlify</a></p>
+      <Weather defaultCity = "Nakuru" />
+      
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,22 +1,73 @@
-import React from "react";
+import React, {useState} from "react";
+import axios from "axios";
 import "./Weather.css";
+import Calendar from "./Calendar";
 
-export default function Weather(){
+export default function Weather(props){
+    const[ready, setReady] = useState(false);
+    const [weatherData, setWeatherData] = useState({});
+    const [city,setCity] = useState(props.defaultCity);
+    function handleResponse(response){
+        console.log(response.data)
+        setWeatherData({
+            temperature: response.data.temperature.feels_like,
+            city: response.data.city,
+            wind: response.data.wind.speed,
+            humidity: response.data.temperature.humidity,
+            iconURl: `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`,
+            description: response.data.condition.description,
+            time: new Date (response.data.time * 1000)
+            
+        })
+
+        setReady(true);
+    }
+    function search() {
+        const apiKey = "83d92b9c8f0fcoa600da63ca304tf074";
+   
+        let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+        axios.get(apiUrl).then(handleResponse);
+    }
+
+    function submitCity(event){
+        event.preventDefault();
+        search(city);
+    }
+    function changeCity(event){
+        setCity(event.target.value);
+    }
+    
+    
+    if(ready){
     return(
         <div className="Weather">
-            <input type="search"/>
-            <input type="submit" value="Search"/>
+            <form className="form" onSubmit={submitCity}>
+                
+                     <input type="search" placeholder="Enter a city..." className="search" onChange={changeCity} />
+                    
+                     <input type="submit" value="Search" className="submit" />
+                    
+                
+            </form>
             <div className="row">
                 <div className="col-6">
-                    <h1>Nairobi</h1>
-                    <p>Sunday 21:28, broken clouds</p>
-                    <p>Humidity: 80%, Wind: 4.79km/h</p>
+                    <h1>{weatherData.city}</h1>
+                    <p className="time"><Calendar time= {weatherData.time}/></p>
+                    <p className="weather-description text-capitalize"> {weatherData.description}</p>
+                    <p className="description">Humidity: <span className="speed">{weatherData.humidity}%</span>, Wind: <span className="speed">{weatherData.wind} km/h</span></p>
                 </div>
                 <div className="col-6">
-                    <p>27°C</p>
+                    <img src={weatherData.iconURl} /> <div className="temp"><span className="degrees">{Math.round(weatherData.temperature)}</span><span className="celcius">°C</span></div>
+                    
                 </div>
             </div>
+            <p className="footer">This project was coded by <a href="https://github.com/jill-7">Jilloh</a> and is <a href="#">open sourced on Github</a> and <a href="#">hosted by Netlify</a></p>
 
         </div>
     )
+    }else{
+    search();
+
+    return "Searching...";
+}
 }
